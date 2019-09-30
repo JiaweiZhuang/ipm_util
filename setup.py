babaeac66dcea92041c966c62a5cc6d73895772e
Jiawei Zhuang <jiaweizhuang@g.harvard.edu>
2019-09-29 22:20:22 -0400

    Correct package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='neuralsolver',
+setup(name='ipm_util',
       version='0.0.1',
       description='Python utilities for Integrated Performance Monitoring (IPM)',
       url='https://github.com/JiaweiZhuang/ipm_util',
